6be7ce9b6cdfef98be8608937b9f2a424b6629b2
DelanoBott <79518609+DelanoBott@users.noreply.github.com>
2022-01-11 15:09:28 +0100

    Add files via upload

--- strings/main.py
+++ strings/main.py
@@ -3,28 +3,32 @@ __winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
 __human_name__ = 'strings'
 
 # Add your code after this line
-scorer_0 = "Gullit"
-scorer_1 = "van Basten"
+scorer_0 = "Ruud Gullit"
+scorer_1 = "Marco van Basten"
 
 goal_0 = 32
 goal_1 = 54
 
-scorers = scorer_0 + str(goal_0), scorer_1 + str(goal_1)
+scorers = scorer_0 + " " + str(goal_0) + ", " + scorer_1 + " " + str(goal_1)
 
-report = "%s scored in the %snd minute." % (
-    scorer_0, goal_0), "%s scored in the %snd minute." % (scorer_1, goal_1)
+report = f"{scorer_0} scored in the {goal_0}nd minute\n{scorer_1} scored in the {goal_1}th minute"
 
 player = "Hans van Breukelen"
-first_name = player[0:4]
-last_name_start = player.find("v")
-last_name = player[last_name_start:]
+first_name_end = player.find(" ")
+first_name = player[0:first_name_end]
+first_name_len = len(first_name)
+last_name_start = player.find(" ")
+last_name = player[last_name_start + 1:]
 last_name_len = len(last_name)
-name_short = player[0:1] + ". " + last_name
-chant = last_name_len * "Hans! "
+name_short = first_name[0:1] + ". " + last_name
+chant_full = first_name_len * (first_name + "!" + " ")
+chant = chant_full[:-1]
+good_chant = chant[:-1] != " "
 
 print(scorers)
 print(report)
 print(first_name)
 print(last_name_len)
 print(name_short)
-print(chant[:-1])
+print(chant)
+print(good_chant)
